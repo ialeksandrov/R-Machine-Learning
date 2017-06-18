23f90188b09959e32bb3ef5543b53db0129eb754
ivan <ialeksandrov1990@abv.bg>
2017-06-18 17:08:25 +0300

    modify the regression model in a simple example with another dataset

--- regression.R
+++ regression.R
@@ -1,15 +1,16 @@
 #!/usr/bin/env Rscript
-# Load the Linkedin dataset
-# Create the days vector
-days <- 1:21
+# Load dataset
+data(longley)
+# fit model
+fit <- lm(Employed~., longley)
 
-# Fit a linear model called on the linkedin views per day: linkedin_lm
-linkedin_lm <- lm(linkedin ~ days)
+# summarize the fit
+summary(fit)
 
-# Predict the number of views for the next three days: linkedin_pred
-future_days <- data.frame(days = 22:24)
-linkedin_pred <- predict(linkedin_lm, future_days)
+# make predictions
+predictions <- predict(fit, longley)
 
-# Plot historical data and predictions
-plot(linkedin ~ days, xlim = c(1, 24))
-points(22:24, linkedin_pred, col = "green")
+
+# summarize accuracy
+mse <- mean((longley$Employed - predictions) ^ 2)
+print(mse)
